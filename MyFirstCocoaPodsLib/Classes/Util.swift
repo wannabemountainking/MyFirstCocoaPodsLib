7aa6aca70b9b6ae35d26437a67356f7db409db70
wannabemountainking <kdy0096@gmail.com>
2025-05-30 16:50:01 +0900

    add birthyear and yourage

--- MyFirstCocoaPodsLib/Classes/Util.swift
+++ MyFirstCocoaPodsLib/Classes/Util.swift
@@ -2,7 +2,16 @@
 import Foundation
 
 public extension Date {
+    
+    var birthYear: Int {
+        return 1975
+    }
+    
     var year: Int {
         return Calendar.current.component(.year, from: Date())
+    }
+    
+    var yourAge: Int {
+        return self.year - birthYear
     }
 }
